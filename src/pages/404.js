efd4af14c1bd8ddb0fce3b3f652d5b4481c15420
Alexander Fedosov <alexander.a.fedosov@gmail.com>
2021-11-09 22:58:38 +0200

    Improve 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,20 @@
 import * as React from "react"
+import Layout from "../components/layout";
 
 const NotFoundPage = () => {
   return (
-    <main>
-      <title>Not found</title>
-      <p>wow, such empty</p>
-    </main>
+    <Layout>
+      <main>
+        <title>Not found</title>
+        <h1>404 Not Found</h1>
+        <blockquote>
+          is a hypertext
+          transfer protocol (HTTP) standard response code, in computer network communications, to indicate that the
+          browser was able to communicate with a given server, but <strong>the server could not find what was requested</strong>.
+          <p><a href="https://en.wikipedia.org/wiki/HTTP_404">https://en.wikipedia.org/wiki/HTTP_404</a></p>
+        </blockquote>
+      </main>
+    </Layout>
   )
 }
 
