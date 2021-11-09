ee61cb20d4a34d582484018b230416045c614223
Alexander Fedosov <alexander.a.fedosov@gmail.com>
2021-11-09 23:01:08 +0200

    Improve home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,9 +9,9 @@ const IndexPage = () => {
         <h1>Hey,</h1>
         <p>welcome to my little digital garden</p>
         <blockquote>
-          A <strong>digital garden</strong> is a collection of notes, highlights, ideas, drafts
+          A <strong>digital garden</strong> is a collection of notes, highlights, ideas and drafts
           not ordered by timestamp like a traditional blog. Anything more than your usual tweet and less than a
-          full-sized blog post
+          full-sized blog post.
         </blockquote>
       </main>
     </Layout>
