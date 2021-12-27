be007c959d81e4da9ff19aa55e9b2698eed7f1b1
Alexander Fedosov <alexander.a.fedosov@gmail.com>
2021-12-27 12:17:06 +0200

    Remove item from wishlist

--- src/pages/wishlist.js
+++ src/pages/wishlist.js
@@ -8,8 +8,8 @@ const WishlistPage = () => {
         <title>Wishlist</title>
         <h1>Wishlist</h1>
         <blockquote>Things, I'd want to buy/read/watch/listen to</blockquote>
-        <p><a href="https://www.objc.io/books/advanced-swift/">[Book] Advanced Swift</a> - $49</p>
         <p><a href="https://www.amazon.de/Funko-57360-POP-Deluxe-Anniversary/dp/B08WCQ4TG7">[Vinyl figure] Harry Pushing Trolley</a> - $49</p>
+        <p><del><a href="https://www.objc.io/books/advanced-swift/">[Book] Advanced Swift</a> - $49</del></p>
       </main>
     </Layout>
   )
